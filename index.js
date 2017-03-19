4ea11b3cd250fe1fa40285c3d5dee6195b75ceba
sholomka <sholomka@gmail.com>
2017-03-19 21:27:45 +0200

    rrr

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var supermodule = require('sholomkasupermodule');
+const http = require('http');
 
-supermodule();
+const server = http.createServer((req, res) => {
+
+
+
+    let url = 'http://google.com';
+
+
+    let body = '<p>Redirecting to <a href="'+ url +'">'+ url +'</a></p>';
+
+    // res.setHeader('Location', url);
+    // res.setHeader('Content-Length', body.length);
+    // res.setHeader('Content-Type', 'text/html');
+
+    res.statusCode = 302;
+
+
+    console.log(res.getHeader('Location'));
+
+
+
+    res.end(body)
+});
+
+server.listen(3000);
